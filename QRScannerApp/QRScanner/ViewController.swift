4e7fa1160e774a52187ced8f8224b85d76549999
Meet Patel <patelmeet0809@gmail.com>
2022-12-24 12:37:33 +0530

    Code upload

---
+++ QRScannerApp/QRScanner/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  QRScanner
+//
+//  Created by Meet Patel on 24/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
